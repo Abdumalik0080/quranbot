728032e57d40dc0bffc5e311b061e8f9a7bc0de3
Abdumalik <windows1020081504@gmail.com>
2025-08-10 22:24:15 +0300

    mana

--- obj/Debug/net8.0/Quranbot.AssemblyInfo.cs
+++ obj/Debug/net8.0/Quranbot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quranbot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e174c474f4fdd267b52d86ea0723bf91a75113d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d00bc58e3a7e1400a06735959c09ae9707fe71ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quranbot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quranbot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
